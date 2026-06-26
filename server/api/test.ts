4fbb956580473924823827c70d9d94c81487bc6d
mccarthyk <mccarthyk@hcflgov.net>
2026-06-26 11:01:46 -0400

    update app

--- server/api/test.ts
+++ server/api/test.ts
@@ -1,20 +1,3 @@
-import contentstack from 'contentstack'
-
-const { Stack, Region } = contentstack
-
 export default defineEventHandler(async (event) => {
-  try {
-    const stack = new Stack({
-      api_key: '',
-      delivery_token: '',
-      environment: '',
-      region: Region.US,
-    })
-
-    console.log({ stack })
-  } catch (error) {
-    console.log({ error })
-  }
-
   return { s: 1 }
 })
